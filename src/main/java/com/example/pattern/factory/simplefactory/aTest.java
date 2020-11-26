1d9b8576c68ff84951d6869539b847db3f0dafe3
zherong_lin <78370412@qq.com>
2020-11-26 15:14:41 +0800

    aTest commit

---
+++ src/main/java/com/example/pattern/factory/simplefactory/aTest.java
@@ -0,0 +1,12 @@
+package com.example.pattern.factory.simplefactory;
+
+/**
+ * Description:
+ *
+ * @author Linzr
+ * @version V1.0.0
+ * @date 2020/11/26 3:14 下午
+ * @since V1.0.0
+ */
+public class aTest {
+}
