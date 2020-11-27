296fbd6aebf4ad2cffcd77a2ade9d7af15324993
zherong_lin <78370412@qq.com>
2020-11-27 09:23:53 +0800

    commit a.java and b.java file 合并提交成功了

---
+++ src/main/java/com/example/pattern/factory/factorymethod/a.java
@@ -0,0 +1,12 @@
+package com.example.pattern.factory.factorymethod;
+
+/**
+ * Description:
+ *
+ * @author Linzr
+ * @version V1.0.0
+ * @date 2020/11/27 9:19 上午
+ * @since V1.0.0
+ */
+public class a {
+}
