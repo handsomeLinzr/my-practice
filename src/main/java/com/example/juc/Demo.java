622a9b7c5ff931d585d0d6043bd349a1cc648cad
zherong_lin <78370412@qq.com>
2021-04-28 18:06:14 +0800

    线程锁提交

---
+++ src/main/java/com/example/juc/Demo.java
@@ -0,0 +1,30 @@
+package com.example.juc;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Description:
+ *
+ * @author Linzr
+ * @version V1.0.0
+ * @date 2021/4/28 5:49 下午
+ * @since V1.0.0
+ */
+public class Demo {
+    private static int i = 0;  // 共享变量
+    public static void inc() {
+        try {
+            Thread.sleep(1);  // 释放CPU资源，其他线程获得
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        i ++;
+    }
+    public static void main(String[] args) throws InterruptedException {
+        for (int j = 0; j < 400; j++) {
+            new Thread(() -> Demo.inc()).start();
+        }
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(i); // 结果为 《=100
+    }
+}
