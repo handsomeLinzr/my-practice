3729279cf95bfeb8add7e0021e2cf748f3898740
Linzr <78370412@qq.com>
2021-06-12 16:52:57 +0800

    内部类默认不会被序列化

--- src/main/java/com/example/serial/v2/Sun.java
+++ src/main/java/com/example/serial/v2/Sun.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class Sun extends Father implements Serializable {
     private static final long serialVersionUID = -5355441881670274372L;
     private String b;
+    private Inner inner;
 
     public String getB() {
         return b;
@@ -13,12 +14,27 @@ public class Sun extends Father implements Serializable {
     public void setB(String b) {
         this.b = b;
     }
+    public Inner getInner() {
+        Inner inner = new Inner();
+        inner.setInnerName("gggfffdgd");
+        return inner;
+    }
+
+    private static class Inner {
+        private String innerName;
+        public void setInnerName(String innerName) {
+            this.innerName = innerName;
+        }
+        public String getInnerName() {
+            return this.innerName;
+        }
+    }
 
     @Override
     public String toString() {
         return "Sun{" +
                 "b='" + b + '\'' +
                 "fatherName='" + getFatherName() + '\''+
-                '}';
+                '}' + "inner:" + inner;
     }
 }
