15ecb6213cfb0ed32385624578314635bc4aed39
林哲荣 <78370412@qq.com>
2022-02-18 13:39:42 +0800

    CycliBarrier 测试

---
+++ src/main/java/com/example/juc/MashibingCycliBarrier.java
@@ -0,0 +1,42 @@
+package com.example.juc;
+
+import java.util.concurrent.*;
+
+/**
+ * Description:
+ *
+ * @author Linzherong
+ * @date 2022/2/18 12:49 下午
+ */
+public class MashibingCycliBarrier {
+
+    private static final ThreadPoolExecutor threadPool = new ThreadPoolExecutor(10,
+            10,
+            60,
+            TimeUnit.SECONDS,
+            new SynchronousQueue<>(),
+            new ThreadFactory() {
+                int i = 0;
+                @Override
+                public Thread newThread(Runnable r) {
+                    return new Thread(r, "my-pool-"+i++ );
+                }
+            });
+
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, ()-> System.out.println(Thread.currentThread().getName()+"======>整合完成"));
+        for (int i = 0; i < 6; i++) {
+            threadPool.execute(() -> {
+                System.out.println("线程执行整合===>" + Thread.currentThread().getName());
+                try {
+                    cyclicBarrier.await();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println("线程执行整合后续===>" + Thread.currentThread().getName());
+            });
+        }
+        threadPool.shutdown();
+    }
+
+}
